bbf8adf838e3507a40ac1f01d317fec5dae54808
MrAndreFelipe <103384593+MrAndreFelipe@users.noreply.github.com>
2023-04-07 12:59:23 -0300

    5th

---
+++ random/src/NarcissisticNumber.java
@@ -0,0 +1,17 @@
+public class NarcissisticNumber {
+
+    //    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+    //    1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+    public boolean isNarcissistic(int number) {
+        String toString = String.valueOf(number);
+        int power = toString.length(), sum = 0;
+
+        for (int i = 0; i < toString.length(); i++) {
+            sum += Math.pow(Integer.parseInt(String.valueOf(toString.charAt(i))), power);
+        }
+        if (number == sum) {
+            return true;
+        }
+        return false;
+    }
+}
--- random/src/app.java
+++ random/src/app.java
@@ -3,9 +3,12 @@ public class app {
     public static void main(String[] args) {
         Mumbling c1 = new Mumbling();
         Reverse5MoreWords c2 = new Reverse5MoreWords();
+        NarcissisticNumber c3 = new NarcissisticNumber();
 
         System.out.println(c1.accum("ZpglnRxqenU"));
         System.out.println(c2.spinWords("Hey wollef sroirraw hi"));
+        System.out.println(c3.isNarcissistic(153));
+
 
     }
 
