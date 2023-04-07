0dd754a100c821bcca9ad7f0688c77ad4bdb7e16
MrAndreFelipe <103384593+MrAndreFelipe@users.noreply.github.com>
2023-04-06 21:43:45 -0300

    2nd

--- random/src/app.java
+++ random/src/app.java
@@ -1,11 +1,11 @@
 public class app {
 
     public static void main(String[] args) {
-        random c1 = new random();
-        morseDecoder morse = new morseDecoder();
+        mumbling c1 = new mumbling();
 
-        System.out.println(c1.squareDigits(765));
-        System.out.println(morse.decoder("... --- ... !   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --."));
+        System.out.println(c1.accum("ZpglnRxqenU"));
+
+
     }
 
 }
--- random/src/mumbling.java
+++ random/src/mumbling.java
@@ -1,2 +1,27 @@
 public class mumbling {
+
+    public String accum(String s) {
+        int iteration = 0;
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            iteration = 0;
+            if (i != 0) {
+                sb.append("-");
+            }
+
+            while (iteration <= i) {
+                if (iteration == 0) {
+                    sb.append(Character.toUpperCase(s.charAt(i)));
+                } else {
+                    sb.append(s.charAt(i));
+                }
+
+                iteration++;
+            }
+
+
+        }
+        return sb.toString();
+    }
 }
